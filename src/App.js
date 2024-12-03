c6eed216ea4371ef24d1ef6a1c65084257172def
zziglet <jjw04033@konkuk.ac.kr>
2024-12-03 16:54:09 +0900

    fix: remove session provider

--- src/App.js
+++ src/App.js
@@ -1,13 +1,29 @@
+import { useEffect } from "react";
 import "./App.css";
 import Router from "./route/Router";
-import { SessionProvider } from "./user/SessionProvider";
 
 function App() {
-  return (
-    <SessionProvider>
-      <Router />
-    </SessionProvider>
-  );
+  useEffect(() => {
+    const fetchSession = async () => {
+      try {
+        const response = await fetch(
+          "http://43.203.219.49:8000/api/users/create-session",
+          {
+            method: "GET"
+          }
+        );
+        if (!response.ok) {
+          throw new Error("세션 요청 실패");
+        }
+      } catch (error) {
+        console.error("세션 요청 중 오류 발생:", error);
+      }
+    };
+
+    fetchSession();
+  }, []);
+
+  return <Router />;
 }
 
 export default App;
