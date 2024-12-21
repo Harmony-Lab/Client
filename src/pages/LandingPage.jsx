e78e773ab4145aa0f6f97e92e1d1fe203646da92
Jiwon Jeong <jjw04033@konkuk.ac.kr>
2024-12-21 20:14:43 +0900

    fix: fix fetchToken

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -95,6 +95,35 @@ function LandingPage() {
   const user = useUser();
   const navigate = useNavigate();
 
+  const fetchToken = async () => {
+    try {
+      const response = await fetch(
+        "http://43.203.219.49:8000/api/users/create-session",
+        {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json"
+          }
+        }
+      );
+
+      if (response.ok) {
+        const data = await response.json();
+        setToken(data.token);
+        user.token = token;
+        console.log("Token obtained:", data.token);
+
+        // 로컬 스토리지에 토큰 저장
+        localStorage.setItem("jwtToken", data.token);
+      } else {
+        const errorData = await response.json();
+        throw new Error(errorData.message);
+      }
+    } catch (error) {
+      console.error("Failed to obtain token:", error);
+    }
+  };
+
   useEffect(() => {
     let timer;
     setToken(user.token);
@@ -147,35 +176,6 @@ function LandingPage() {
       console.error("Error analyzing emotion:", error);
     } finally {
       setIsLoading(false);
-    }
-  };
-
-  const fetchToken = async () => {
-    try {
-      const response = await fetch(
-        "http://43.203.219.49:8000/api/users/create-session",
-        {
-          method: "GET",
-          headers: {
-            "Content-Type": "application/json"
-          }
-        }
-      );
-
-      if (response.ok) {
-        const data = await response.json();
-        setToken(data.token);
-        user.token = token;
-        console.log("Token obtained:", data.token);
-
-        // 로컬 스토리지에 토큰 저장
-        localStorage.setItem("jwtToken", data.token);
-      } else {
-        const errorData = await response.json();
-        throw new Error(errorData.message);
-      }
-    } catch (error) {
-      console.error("Failed to obtain token:", error);
     }
   };
 
